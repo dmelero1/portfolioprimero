3c59b33a784adeea8a9b556adf3451f981f75b49
dmelero1 <danielmm1103@gmail.com>
2024-12-10 10:44:38 +0100

    Función Cambio Img Projects

--- portfolioprimero/src/main.js
+++ portfolioprimero/src/main.js
@@ -1,1 +1,14 @@
 console.log('Vite está funcionando correctamente');
+
+const images = ["./src/assets/imagenWebDiseños.jpg", "./src/assets/paginaCoches.jpg", "./src/assets/paginaCiudades.jpg"];
+let currentIndex = 0;
+
+const mainImage = document.getElementById("main-image");
+const nextButton = document.getElementById("next-button");
+
+nextButton.addEventListener("click", () => {
+  currentIndex = (currentIndex + 1) % images.length;
+  mainImage.src = images[currentIndex];
+  mainImage.style.transform = "scale(1.1)";
+  setTimeout(() => (mainImage.style.transform = "scale(1)"), 300);
+});
