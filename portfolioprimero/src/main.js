c921969aa04e353cef9d1b2ea86ce8f4684c79f2
dmelero1 <danielmm1103@gmail.com>
2024-12-12 13:55:02 +0100

    Mejora enlaces botón

--- portfolioprimero/src/main.js
+++ portfolioprimero/src/main.js
@@ -11,13 +11,13 @@ const images = [
   src: "./src/assets/paginaCoches.jpg",
   title: "Concesionario:",
   description: "He creado una pagina que simula un concesionario de coches que incluye un filtrado por marcas o caracteristicas del coche como los kilometros, el año, el precio, etc...Incluye la opcion de valorar con un like cada coche.",
-  link: "https://example.com/graphic"
+  link: "https://github.com/dmelero1/concesionario"
 },
 {
   src: "./src/assets/paginaCiudades.jpg",
   title: "Ciudades Patrimonio:",
   description: "He creado una pagina donde aparecen muchas ciudades de España con la opcion de filtrar su busqueda por las rutas de estas ciudades. Incluye la opcion de valorar del 1 al 5 estrellas cada ruta.",
-  link: "https://example.com/app"
+  link: "https://github.com/dmelero1/ciudadesPatrimonio"
 }
 ];
 let currentIndex = 0;
@@ -27,14 +27,14 @@ const nextButton = document.getElementById("next-button");
 const previousButton = document.getElementById("previous-button");
 const imageTitle = document.getElementById("image-title");
 const imageDescription = document.getElementById("image-description");
-const imageLink = document.getElementById("image-link");
+const dynamicLink = document.getElementById("dynamic-link");
 
 function updateContent() {
   const currentData = images[currentIndex];
   mainImage.src = currentData.src;
   imageTitle.textContent = currentData.title;
   imageDescription.textContent = currentData.description;
-  imageLink.href = currentData.link;
+  dynamicLink.href = currentData.link;
 
   mainImage.style.transform = "scale(1.1)";
   setTimeout(() => (mainImage.style.transform = "scale(1)"), 300);
